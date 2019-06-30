e53a34f73eba0b1524a5a658a17d078008444bdb
Ilija Kocic <ilija.kocic@gmail.com>
2019-06-30 10:20:50 +0200

    For Loop instead of 4 separate NEXT_DIGITs

--- TGI1_PrakUE4_ZahlenschlossV7.asm
+++ TGI1_PrakUE4_ZahlenschlossV7.asm
@@ -3,8 +3,8 @@
 *
 *  Created: 6/16/2019 9:25:12 PM
 *  Author: Ilija K. & Josi Brauer
-*  Version: 6
-*/ 
+*  Version: 7
+*/
 
 ; Variable Declaration
 .def Digit = R20
@@ -14,6 +14,7 @@
 ; Own Variables
 .def InputCount = R24
 .def LastState = R25
+.def Cntr = R17
 .DSEG
 .ORG $0100
 ; Deprecated CODE
@@ -22,13 +23,13 @@
 ;.ORG $0000
 
 ;TODO: USE TRI-STATE PORTX ADDITIONALLY TO DDRX!!!
-; Init. of PortA,B,D 
+; Init. of PortA,B,D
 LDI Temp, 0b01111110	;PA7,PA0: IN
 OUT DDRA, Temp
 ;OUT PORTA, Temp			;Tristate
 
 LDI Temp, 0b11111111	;PB0-5: OUT
-OUT DDRB, Temp		
+OUT DDRB, Temp
 
 LDI Temp, 0b11111111	;PD0-3: OUT
 OUT DDRD, Temp
@@ -46,62 +47,35 @@ CLR LastState
 MAIN:
 	CLR Fail
 	CLR Temp
-	
+
 	LDI ZH, high(CODE<<1)	;No idea what '<<' is for!?
 	LDI ZL, low(CODE<<1)
 	;4x
 
-;(NEXT_DIGIT0)
-	LDI InputCount, 0x01
-	OUT PORTB, InputCount	
-	CALL POLL_BUTTONS
-	;Compr
-	LPM Temp, Z+
-	;CP Digit, Temp ;Deprecated
-	CPSE Digit, Temp
-	INC Fail
-
-;NEXT_DIGIT1:
-	LDI InputCount, 0b00000011
+; LoopCounter
+	LDI Cntr, 0x04
+	CLR InputCount				;Lower Nibble is the one displayed
+FORLOOP:
+	SEC							; Set Carry
+	ROL InputCount				; Successively adds 1s at the end
 	OUT PORTB, InputCount
 	CALL POLL_BUTTONS
-	;Compr
-	LPM Temp, Z+ ;Temp should be "CodeDigit"
-	;CP Digit, Temp ;Deprecated
-	CPSE Digit, Temp
-	INC Fail
-
-;NEXT_DIGIT2:
-	LDI InputCount, 0b00000111
-	OUT PORTB, InputCount
-	CALL POLL_BUTTONS
-	;Compr
+	;Comparison
 	LPM Temp, Z+
-	;CP Digit, Temp ; Depr.
 	CPSE Digit, Temp
 	INC Fail
-
-;NEXT_DIGIT3:
-	LDI InputCount, 0b00001111
-	OUT PORTB, InputCount
-	CALL POLL_BUTTONS
-	;Compr
-	LPM Temp, Z+
-	;CP Digit, Temp ;Deprecated
-	CPSE Digit, Temp
-	INC Fail
-	
-
+	DEC Cntr
+	BRNE FORLOOP
 ;CHECK_FAIL:
-	TST Fail			;Activates Z- or N-Flag
+	TST Fail					;Activates Z- or N-Flag
 	BREQ GREEN_LIGHT			;Reacts to Z-Flag
 	LDI Temp, 0b00101111
-	OUT PORTB, Temp		;Redlight
+	OUT PORTB, Temp				;Redlight
 	JMP NEXT
 
 GREEN_LIGHT:
 	LDI Temp, 0b00011111
-	OUT PORTB, Temp; DDRB 
+	OUT PORTB, Temp; DDRB
 
 NEXT:
 	CALL WAIT_ENTER
@@ -110,7 +84,7 @@ NEXT:
 WAIT_KEYPRESS:
 	MOV Temp, ButtonStates	;Save prev.
 	IN ButtonStates, PORTA
-	CP Temp, ButtonStates 
+	CP Temp, ButtonStates
 	BRCS EXIT_KEYPRESS
 	JMP WAIT_KEYPRESS
 
@@ -120,9 +94,9 @@ EXIT_KEYPRESS:
 WAIT_ENTER:
 	;MOV Temp, ButtonStates	;Save prev. ;Deprecated prolly 'cuz of Tristate
 	IN ButtonStates, PINA
-	
+
 	ANDI ButtonStates, 0b10000000
-	;CP Temp, ButtonStates 
+	;CP Temp, ButtonStates
 	BREQ WAIT_ENTER
 	RET
 
@@ -130,9 +104,9 @@ WAIT_ENTER:
 WAIT_INCREMENTOR:
 	MOV Temp, ButtonStates	;Save prev.
 	IN ButtonStates, PORTA
-	
+
 	ANDI ButtonStates, 0b00000001
-	CP Temp, ButtonStates 
+	CP Temp, ButtonStates
 	INC DIGIT
 	JMP WAIT_INCREMENTOR
 
@@ -147,7 +121,7 @@ POLL_BUTTONS:
 	IN ButtonStates, PINA
 POLL_LOOP:
 	IN Temp, PINA		;Read Raw-Input
-	
+
 	SBRS ButtonStates, 0		; Check for Incrmntr-Input
 	RJMP NEXT_IF
 	SBRS Temp, 0
@@ -165,19 +139,17 @@ SKIP1:
 
 INCREMENTOR:
 	INC Digit
-	
+
 	LDI Temp, 10				;Req. For Modulo Base 10
 	CP Digit, Temp
-	BRNE SKIP_SUB					
+	BRNE SKIP_SUB
 	SUBI Digit, 10
-	
+
 	;Implicit Return statement because of end
 
 SKIP_SUB:
 	out PortD, Digit
 	RET
-				
+
 ; //////////////END///////////////
 CODE: .DB 0, 8, 1, 5	;Previously @ DSEG
-
-
